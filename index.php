294a05fb322471dac449e94bf3c3b4a244dc5424
luciano herdeli <luciano.herdeli5@gmail.com>
2022-09-23 19:27:29 -0300

    index y valir

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Servicio Meteorológico</title>
+</head>
+<body>
+     <h1> Servicio Meteorológico</h1>
+    <form action="validar.php" method="post">
+    <input type="date" name="date" required >
+    <input type="submit" name="consulta" value="Enviar Datos">
+ <br>
+  </form>
+</body>
+</html>
---
+++ valirdar.php
@@ -0,0 +1,5 @@
+<?php
+$dia=$_POST[date];
+setlocale(LC_ALL,'es_es.UTF-8');
+date_default_timezone_set("America/Argentina/Buenos_Aires");
+?>
