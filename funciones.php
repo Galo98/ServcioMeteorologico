4921fb16cce99e0414032cf35f1ba5b177bd11f1
Galo98 <galonahuel98@gmail.com>
2022-09-30 15:40:35 -0300

    refactorizando codigo de ingreso, aplicando funciones

---
+++ funciones.php
@@ -0,0 +1,94 @@
+<?php 
+/*
+31 enero,marzo,mayo,julio,agosto,octubre,diciembre
+30 abril,junio,septiembre,noviembre
+28 febrero
+    */
+
+    #region Funcion mes de 31 dias
+function crearMesLargo(){
+    for($i = 1; $i <= 31; $i++){
+?> 
+        <label for="lluvia<?php echo $i;?>">
+            Ingrese la cantidad llovida para el 
+            <?php if($i < 10){
+                echo "0" .$i;
+            }else{
+            echo $i;
+            }?>/1/2022
+
+            <input type="number" step="0.01" name="lluvia[]" id="lluvia<?php echo$i;?>">
+            </label>
+        <?php
+            }
+        ?>
+        <button>Guardar registros</button>
+<?php    
+        }
+        #endregion
+    
+    #region Funcion mes de 30 dias
+    function crearMesMedio(){
+    for($i = 1; $i <= 30; $i++){
+?> 
+        <label for="lluvia<?php echo $i;?>">
+            Ingrese la cantidad llovida para el 
+            <?php if($i < 10){
+                echo "0" .$i;
+            }else{
+            echo $i;
+            }?>/1/2022
+
+            <input type="number" step="0.01" name="lluvia[]" id="lluvia<?php echo$i;?>">
+            </label>
+        <?php
+            }
+        ?>
+        <button>Guardar registros</button>
+<?php    
+        }
+    #endregion
+
+    #region Funcion mes de 28 dias
+    function crearMesCorto(){
+    for($i = 1; $i <= 28; $i++){
+?> 
+        <label for="lluvia<?php echo $i;?>">
+            Ingrese la cantidad llovida para el 
+            <?php if($i < 10){
+                echo "0" .$i;
+            }else{
+            echo $i;
+            }?>/1/2022
+
+            <input type="number" step="0.01" name="lluvia[]" id="lluvia<?php echo$i;?>">
+            </label>
+        <?php
+            }
+        ?>
+        <button>Guardar registros</button>
+<?php    
+        }
+    #endregion
+
+    #region Funcion formulario completo
+    function crearForumario($mostrar){
+        
+        $mes = array(" ","enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre");
+        for($i = 1; $i <= 12; $i++){
+            ?>
+            <form method="GET">
+            <input type="hidden" name="mes" value="<?php echo $i;?>">
+            <button> Mes de <?php echo $mes[$i]; ?> / 2022</button>
+        </form>
+        <form class="deslisable">
+            <?php 
+            if($mostrar == $i){
+                crearMesLargo();
+            }
+            ?>
+        </form>
+        <?php }
+    }
+    #endregion
+?>
--- nuevoingreso.php
+++ nuevoingreso.php
@@ -1,4 +1,5 @@
 <?php 
+    require "funciones.php";
     $mostrar = 0;
     if(isset($_GET['mes']) && $_GET['mes'] >= 1){
         $mostrar = $_GET['mes'];
@@ -27,36 +28,7 @@
     </header>
     <main>
         <h1>Seleccione el mes en el que desea ingresar nuevos datos</h1>
-        <form action="" method="GET">
-            <input type="hidden" name="mes" value="1">
-            <button> Mes de Enero / 2022</button>
-        </form>
-        <form action="" class="deslisable">
-        <?php 
-        if($mostrar == 1){
-            for($i = 1; $i <= 31; $i++){
-        ?> 
-            <label for="lluvia<?php echo $i;?>">Ingrese la cantidad llovida para el <?php 
-            if($i < 10){
-                echo "0" .$i;
-            }else{
-            echo $i;
-            }?>/1/2022
-            <input type="number" step="0.01" name="lluvia[]" id="lluvia<?php echo$i;?>">
-            </label>
-        <?php
-            }
-        }
-        ?>
-        </form>
-        <form action="" method="GET">
-            <input type="hidden" name="mes" value="2">
-            <button> Mes de Febrero / 2022</button>
-        </form>
-        <form action="" method="GET">
-            <input type="hidden" name="mes" value="3">
-            <button> Mes de Marzo / 2022</button>
-        </form>
+        <?php crearForumario($mostrar);?>
     </main>
 </body>
 </html>
